0828100bc799cd4104b0fc8ae0d106e899b58521
dasdevpro <makkambaevdastan@gmail.com>
2025-12-11 02:50:58 +0600

    style: delete RepositoryRestResource

--- domain/src/main/java/kg/management/datum/domain/repository/user/UserRepository.java
+++ domain/src/main/java/kg/management/datum/domain/repository/user/UserRepository.java
@@ -4,13 +4,11 @@ import kg.management.datum.domain.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.history.RevisionRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-@RepositoryRestResource(collectionResourceRel = "user", itemResourceRel = "user", path = "user")
 public interface UserRepository extends JpaRepository<User, Long>,
         RevisionRepository<User, Long, Long>,
         JpaSpecificationExecutor<User> {
